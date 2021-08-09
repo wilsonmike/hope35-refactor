6d18bdc87ba3388b90b7ce65519a956bc7971f39
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-09 17:08:00 -0400

    eod commit

--- paper-kit-2-angular-master/src/app/examples/hopehome/hopehome.component.ts
+++ paper-kit-2-angular-master/src/app/examples/hopehome/hopehome.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { ViewChild } from "@angular/core";
+import {
+  NgbCarousel,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-hopehome",
@@ -6,7 +12,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./hopehome.component.css"],
 })
 export class HopehomeComponent implements OnInit {
+  images = [
+    {
+      imageURL: "../../../assets/img/ilya-yakover.jpg",
+    },
+    {
+      imageURL: "../../../assets/img/bruno-abatti.jpg",
+    },
+  ];
+
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild("carousel", { static: true }) carousel: NgbCarousel;
+
   constructor() {}
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 
   ngOnInit(): void {}
 
