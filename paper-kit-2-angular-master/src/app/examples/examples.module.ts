af58de98bfc0ed03c99b93c9f73fdbce7bd9ba8d
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-12 13:15:06 -0400

    cleaned up components && fixed nav

--- paper-kit-2-angular-master/src/app/examples/examples.module.ts
+++ paper-kit-2-angular-master/src/app/examples/examples.module.ts
@@ -1,38 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { LandingComponent } from './landing/landing.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SignupComponent } from './signup/signup.component';
-import { HopehomeComponent } from './hopehome/hopehome.component';
-import { FamilyfriendsComponent } from './familyfriends/familyfriends.component';
-import { GetstartedComponent } from './getstarted/getstarted.component';
-import { AboutComponent } from './about/about.component';
-import { UploadManagerComponent } from './upload-manager/upload-manager.component';
-import { UploadTaskComponent } from './upload-task/upload-task.component';
-import { DropzoneDirective } from './directives/dropzone.directive';
-import { MainformControllerComponent } from './mainform-controller/mainform-controller.component';
+import { LandingComponent } from "./landing/landing.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { SignupComponent } from "./signup/signup.component";
+import { HopehomeComponent } from "./hopehome/hopehome.component";
+import { FamilyfriendsComponent } from "./familyfriends/familyfriends.component";
+import { GetstartedComponent } from "./getstarted/getstarted.component";
+import { AboutComponent } from "./about/about.component";
+import { DropzoneDirective } from "./directives/dropzone.directive";
+import { MainformControllerComponent } from "./mainform-controller/mainform-controller.component";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        NgbModule,
-    ],
-    declarations: [
-        LandingComponent,
-        SignupComponent,
-        ProfileComponent,
-        HopehomeComponent,
-        FamilyfriendsComponent,
-        GetstartedComponent,
-        AboutComponent,
-        UploadManagerComponent,
-        UploadTaskComponent,
-        DropzoneDirective,
-        MainformControllerComponent
-    ]
+  imports: [CommonModule, FormsModule, NgbModule],
+  declarations: [
+    LandingComponent,
+    SignupComponent,
+    ProfileComponent,
+    HopehomeComponent,
+    FamilyfriendsComponent,
+    GetstartedComponent,
+    AboutComponent,
+    DropzoneDirective,
+    MainformControllerComponent,
+  ],
 })
-export class ExamplesModule { }
+export class ExamplesModule {}
--- paper-kit-2-angular-master/src/app/shared/navbar/navbar.component.ts
+++ paper-kit-2-angular-master/src/app/shared/navbar/navbar.component.ts
@@ -1,82 +1,95 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Component, OnInit, ElementRef } from "@angular/core";
+import { HostListener } from "@angular/core";
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy,
+} from "@angular/common";
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
-    private toggleButton: any;
-    private sidebarVisible: boolean;
-    activateHideLogo: any;
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  activateHideLogo: any;
 
-    constructor(public location: Location, private element : ElementRef) {
-        this.sidebarVisible = false;
-    }
+  constructor(public location: Location, private element: ElementRef) {
+    this.sidebarVisible = false;
+  }
 
-    ngOnInit() {
-        const navbar: HTMLElement = this.element.nativeElement;
-        this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
-    }
-    sidebarOpen() {
-        const toggleButton = this.toggleButton;
-        const html = document.getElementsByTagName('html')[0];
-        // console.log(html);
-        // console.log(toggleButton, 'toggle');
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName("navbar-toggler")[0];
+  }
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName("html")[0];
+    // console.log(html);
+    // console.log(toggleButton, 'toggle');
 
-        setTimeout(function(){
-            toggleButton.classList.add('toggled');
-        }, 500);
-        html.classList.add('nav-open');
+    setTimeout(function () {
+      toggleButton.classList.add("toggled");
+    }, 500);
+    html.classList.add("nav-open");
 
-        this.sidebarVisible = true;
-    };
-    sidebarClose() {
-        const html = document.getElementsByTagName('html')[0];
-        // console.log(html);
-        this.toggleButton.classList.remove('toggled');
-        this.sidebarVisible = false;
-        html.classList.remove('nav-open');
-    };
-    sidebarToggle() {
-        // const toggleButton = this.toggleButton;
-        // const body = document.getElementsByTagName('body')[0];
-        if (this.sidebarVisible === false) {
-            this.sidebarOpen();
-        } else {
-            this.sidebarClose();
-        }
-    };
-    isHome() {
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      if(titlee.charAt(0) === '#'){
-          titlee = titlee.slice( 1 );
-      }
-        if( titlee === '/home' ) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    this.sidebarVisible = true;
+  }
+  sidebarClose() {
+    const html = document.getElementsByTagName("html")[0];
+    // console.log(html);
+    this.toggleButton.classList.remove("toggled");
+    this.sidebarVisible = false;
+    html.classList.remove("nav-open");
+  }
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
     }
-    isDocumentation() {
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      if(titlee.charAt(0) === '#'){
-          titlee = titlee.slice( 1 );
-      }
-        if( titlee === '/documentation' ) {
-            return true;
-        }
-        else {
-            return false;
-        }
+  }
+  isHome() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === "#") {
+      titlee = titlee.slice(1);
     }
-    activateHide() {
-        if (window.scrollY > 1) {
-            this.activateHideLogo = true;
-        } else {
-            this.activateHideLogo = false;
-        }
+    if (titlee === "/home") {
+      return true;
+    } else {
+      return false;
     }
+  }
+  isDocumentation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === "#") {
+      titlee = titlee.slice(1);
+    }
+    if (titlee === "/documentation") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  activateHide() {
+    if (window.scrollY > 1) {
+      this.activateHideLogo = true;
+    } else {
+      this.activateHideLogo = false;
+    }
+  }
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    let element = document.querySelector(".navbar") as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add("navbar-inverse");
+    } else {
+      element.classList.remove("navbar-inverse");
+    }
+  }
 }
